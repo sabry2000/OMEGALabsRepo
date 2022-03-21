6a95352fd60f2030093fccf4b294f61a05d5c6de
Asabry <Ahmed.Sabry@Kardium.com>
2022-03-21 08:25:22 -0700

    setting number of pulses functionality

--- tb6600-embedded/tb6600-embedded.ino
+++ tb6600-embedded/tb6600-embedded.ino
@@ -1,10 +1,18 @@
 const int PULSE_PIN = 2;
 const int ENABLE_PIN = 3;
 const int DIRECTION_PIN = 4;
-const int PULSE_PERIOD_US = 50;
-const int NUMBER_OF_PULSES = 6400;
+const int PULSE_HALF_PERIOD_US = 50;
 
-int incomingByte = 0; // for incoming serial data
+const char UP_COMMAND = 'u';
+const char DOWN_COMMAND = 'd';
+const char SET_NUMBER_OF_PULSES_COMMAND = 'p';
+
+
+int NUMBER_OF_PULSES = 6400;
+
+String commandString; // for incoming serial data
+char command;
+String commandArgument;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,38 +32,67 @@ void loop() {
   // send data only when you receive data:
   if (Serial.available() > 0) {
     // read the incoming byte:
-    incomingByte = Serial.read();
-
-    if (incomingByte == 'u')
-    {
-      GoUp();
+    commandString = Serial.readString();
+    command = commandString.charAt(0);
+    
+    int spaceIndex = commandString.indexOf(' ');
+    if (spaceIndex != -1){
+        commandArgument = commandString.substring(spaceIndex);
     }
-    else if (incomingByte == 'd')
-    {
-      GoDown();
+
+    switch (command) {
+      case UP_COMMAND:
+        GoUp();
+        break;
+      case DOWN_COMMAND:
+        GoUp();
+        break;
+      case SET_NUMBER_OF_PULSES_COMMAND:
+//        String numberOfPulsesString = Serial.readString();
+//        
+//        for (int i = numberOfPulsesString.length() - 1; i >= 0; i--){
+//          char character = numberOfPulsesString.charAt(i);
+//          if (character == ' ')
+//            numberOfPulsesString.remove(i);
+//        }
+//        int numberOfPulses =  numberOfPulsesString.parseInt(); 
+
+        int numberOfPulses = commandArgument.toInt();
+        SetNumberOfPulses(numberOfPulses);
+        break;
+      default:
+      // clear input buffer;
+        while (Serial.read() >= 0)
+          ; // do nothing
+        break;
     }
   }
 }
 
 void GoUp() {
-  for (int i = 0; i < NUMBER_OF_PULSES; i++) //Backward 5000 steps
+  digitalWrite(DIRECTION_PIN, HIGH);
+  
+  for (int i = 0; i < NUMBER_OF_PULSES; i++)
   {
-    digitalWrite(DIRECTION_PIN, HIGH);
     digitalWrite(PULSE_PIN, HIGH);
-    delayMicroseconds(PULSE_PERIOD_US);
+    delayMicroseconds(PULSE_HALF_PERIOD_US);
     digitalWrite(PULSE_PIN, LOW);
-    delayMicroseconds(PULSE_PERIOD_US);
+    delayMicroseconds(PULSE_HALF_PERIOD_US);
   }
 }
 
 void GoDown() {
-  Serial.println("DOWN");
-  for (int i = 0; i < NUMBER_OF_PULSES; i++) //Backward 5000 steps
+  digitalWrite(DIRECTION_PIN, LOW);
+  
+  for (int i = 0; i < NUMBER_OF_PULSES; i++)
   {
-    digitalWrite(DIRECTION_PIN, LOW);
     digitalWrite(PULSE_PIN, HIGH);
-    delayMicroseconds(PULSE_PERIOD_US);
+    delayMicroseconds(PULSE_HALF_PERIOD_US);
     digitalWrite(PULSE_PIN, LOW);
-    delayMicroseconds(PULSE_PERIOD_US);
+    delayMicroseconds(PULSE_HALF_PERIOD_US);
   }
+}
+
+void SetNumberOfPulses(int numberOfPulses){
+  NUMBER_OF_PULSES = numberOfPulses;
 }
