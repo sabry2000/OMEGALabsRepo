b2576b6526c4c5d31661af6f88dff1eade534e4a
Asabry <52863189+sabry2000@users.noreply.github.com>
2022-06-14 14:19:48 -0700

    adding structure for rocky

---
+++ DeviceLibrary/Rocky100.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceLibrary
+{
+    public class Rocky100
+    {
+
+        private sendCommand(RW rw, )
+        {
+            if (rw == RW.Read)
+        }
+    }
+}
